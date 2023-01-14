0642d93532bf49e457934ad5bafa5a640b02b27a
AAJLI Ahmed <ahmed.conceptor@gmail.com>
2023-01-14 16:09:45 +0100

    add hashMap class

--- LoginSystem/src/IDandPasswords.java
+++ LoginSystem/src/IDandPasswords.java
@@ -1,4 +1,11 @@
+import java.util.HashMap;
 
 public class IDandPasswords {
-
+	HashMap<String,String> logininfo = new HashMap<String, String>();
+	
+	public IDandPasswords() {
+		logininfo.put("Bro", "pizza");
+		logininfo.put("Ahmed", "aajli");
+		logininfo.put("BroCode", "abc123");
+	}
 }
